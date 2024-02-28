df665a9359922d553c50ecfb769a917d0b698ad7
Dakas-1 <dakas9521@gmail.com>
2024-02-28 21:52:02 +0300

    formatting

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class HomepageController extends AbstractController
 {
-    #[Route('/index')]
-public function index(): Response
-{
-return $this->render('index-teacher.html.twig');
-}
+    #[Route('/', name: 'app_homepage')]
+    public function index(): Response
+    {
+        return $this->render('index-teacher.html.twig');
+    }
 }
