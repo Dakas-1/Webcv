a30572c579686df06eed19e34975c16a1e088875
Dakas-1 <dakas9521@gmail.com>
2024-03-01 23:35:12 +0300

    add method getFunctions

--- src/TwigExtensions/ImageExtension.php
+++ src/TwigExtensions/ImageExtension.php
@@ -5,6 +5,7 @@ namespace App\TwigExtensions;
 use Twig\Extension\AbstractExtension;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Filesystem\Filesystem;
+use Twig\TwigFunction;
 
 class ImageExtension extends AbstractExtension
 {
@@ -13,11 +14,15 @@ class ImageExtension extends AbstractExtension
                                 private $absolutePath
     )
     {}
-
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('image_exist', [$this, 'imageExists']),
+        ];
+    }
     public function imageExists(string $imageName): bool
     {
         $filesystem = new Filesystem();
         return $filesystem->exists($this->absolutePath . $imageName);
-
     }
 }
