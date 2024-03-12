5b146fa86cca794c9a2f25bf614baa96ace7679d
Dakas-1 <dakas9521@gmail.com>
2024-03-12 17:13:17 +0300

    format fix

--- src/TwigExtensions/ImageExtension.php
+++ src/TwigExtensions/ImageExtension.php
@@ -9,19 +9,21 @@ use Twig\TwigFunction;
 
 class ImageExtension extends AbstractExtension
 {
-
-    public function __construct(#[Autowire('%kernel.debug%')]
-                                private $absolutePath,
-                                private FileSystem
-                                $filesystem
+    public function __construct(
+        #[Autowire('%kernel.debug%')]
+        private            $absolutePath,
+        private FileSystem $filesystem
     )
-    {}
+    {
+    }
+
     public function getFunctions(): array
     {
         return [
             new TwigFunction('image_exists', [$this, 'imageExists']),
         ];
     }
+
     public function imageExists(string $imageName): bool
     {
         return $this->filesystem->exists($this->absolutePath . $imageName);
