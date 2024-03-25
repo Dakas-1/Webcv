84b6a61f4794aaf3a937cdd623b8933296f9d2d9
Dakas-1 <dakas9521@gmail.com>
2024-03-25 19:49:35 +0300

    implements PasswordAuthenticatedUserInterface

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity]
 #[ORM\Table(name: "users")]
-class User implements UserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: "AUTO")]
