7ea45976656d77be35c5ee574e7634f341876671
Dakas-1 <dakas9521@gmail.com>
2024-07-16 12:46:57 +0300

    added fields: name, slug

--- src/Controller/Admin/SkillCrudController.php
+++ src/Controller/Admin/SkillCrudController.php
@@ -2,48 +2,45 @@
 
 namespace App\Controller\Admin;
 
+use App\EasyAdmin\Field\TranslationsSimpleField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use App\Event\SkillEvent;
 use App\Entity\Skill;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class SkillCrudController extends AbstractCrudController
 {
-    private EventDispatcherInterface $eventDispatcher;
-
-    public function __construct(EventDispatcherInterface $eventDispatcher)
-    {
-        $this->eventDispatcher = $eventDispatcher;
-    }
-
     public static function getEntityFqcn(): string
     {
         return Skill::class;
     }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            'name',
-        ];
+        yield TranslationsSimpleField::new('translations', null, [
+            'name' => [
+                'field_type' => TextType::class,
+                'required' => true,
+            ],
+        ]);
+        yield Field::new('slug', 'Slug')
+            ->setFormType(TextType::class)
+            ->setRequired(true)
+            ->hideOnIndex();
     }
 
     public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
     {
         if ($entityInstance instanceof Skill) {
-            $event = new SkillEvent($entityInstance);
-            $this->eventDispatcher->dispatch($event, SkillEvent::NAME);
-
             $entityManager->persist($entityInstance);
             $entityManager->flush();
         }
     }
+
     public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
     {
         if ($entityInstance instanceof Skill) {
-            $event = new SkillEvent($entityInstance);
-            $this->eventDispatcher->dispatch($event, SkillEvent::NAME);
-
             $entityManager->persist($entityInstance);
             $entityManager->flush();
         }
