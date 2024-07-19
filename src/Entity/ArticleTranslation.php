4e9377e15f792f780908931e00f64d77f45af3e3
Dakas-1 <dakas9521@gmail.com>
2024-07-19 21:17:59 +0300

    created translation

---
+++ src/Entity/ArticleTranslation.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslationInterface;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslationTrait;
+
+#[ORM\Entity]
+#[ORM\Table(name: "article_translations")]
+class ArticleTranslation implements TranslationInterface
+{
+    use TranslationTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private int $id;
+
+    #[ORM\Column(type: "text", length: 255)]
+    private string $text;
+
+    #[ORM\Column(type: "string", length: 255,)]
+    private string $title;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): void
+    {
+        $this->locale = $locale;
+    }
+}
