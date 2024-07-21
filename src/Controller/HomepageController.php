4b07b04163a402a2f56c11a3344c1f9ebc150d90
Dakas-1 <dakas9521@gmail.com>
2024-07-21 23:24:44 +0300

    brought to the stream

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,12 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\SkillTranslation;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('index-teacher.html.twig');
+        $skills = $entityManager->getRepository(SkillTranslation::class)->findAll();
+
+        if (!$skills) {
+            throw $this->createNotFoundException('The skill does not exist');
+        }
+
+        return $this->render('index-teacher.html.twig', [
+            'skill' => $skills,
+        ]);
     }
 }
