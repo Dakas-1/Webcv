7a1dc377196d851525c6e1e1633de398fb4022e0
Dakas-1 <dakas9521@gmail.com>
2024-07-22 20:52:05 +0300

    passed the data to the template

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,21 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\SkillTranslation;
+use App\Entity\Skill;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        $skills = $entityManager->getRepository(SkillTranslation::class)->findAll();
+        $skills = $entityManager->getRepository(Skill::class)->findAll();
 
         if (!$skills) {
             throw $this->createNotFoundException('The skill does not exist');
         }
 
         return $this->render('index-teacher.html.twig', [
-            'skill' => $skills,
+            'skills' => $skills,
         ]);
     }
 }
