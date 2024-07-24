9effd090e76a4a48660254c13232cde1105c439b
Dakas-1 <dakas9521@gmail.com>
2024-07-24 03:38:47 +0300

    added url generation depending on locale

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Skill;
 
 class HomepageController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
-    public function index(EntityManagerInterface $entityManager): Response
+    #[Route('/{_locale}', name: 'app_homepage', requirements: ['_locale' => 'en|ru|'])]
+    public function index(EntityManagerInterface $entityManager, Request $request, UrlGeneratorInterface $urlGenerator): Response
     {
         $skills = $entityManager->getRepository(Skill::class)->findAll();
 
@@ -19,8 +21,25 @@ class HomepageController extends AbstractController
             throw $this->createNotFoundException('The skill does not exist');
         }
 
+        if ($request->getLocale() === 'en') {
+            $newLocale = 'ru';
+        }elseif($request->getLocale() === 'ru'){
+            $newLocale = 'en';
+        }else{
+            $newLocale = null;
+        }
+
+        if ($newLocale !== null) {
+            $url = $urlGenerator->generate('app_homepage', ['_locale' => $newLocale]);
+        } else {
+            $url = $urlGenerator->generate('app_homepage');
+        }
+
+        $fullUrl = $request->getSchemeAndHttpHost() . $url;
+
         return $this->render('index-teacher.html.twig', [
             'skills' => $skills,
+            'fullUrl' => $fullUrl,
         ]);
     }
 }
